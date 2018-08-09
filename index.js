dd2151ed9aa175b8667baddfed4e6a111f6b82ff
shivangi <shivangi.das7@gmail.com>
2018-08-10 00:34:54 +0530

    got your nose

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 const imageScaleFactor = 0.5;
 const outputStride = 16;
 const flipHorizontal = false;
-let imageData = document.getElementById("cat");
+let imageData = document.getElementById("image");
+let canvas = document.getElementById("poseCanvas");
+let context = canvas.getContext("2d");
+canvas.height = imageData.height;
+canvas.width = imageData.width;
 posenet
   .load()
   .then(function(net) {
@@ -15,4 +19,14 @@ posenet
   })
   .then(function(pose) {
     console.log(pose);
+
+    context.fillRect(
+      pose.keypoints[0].position.x,
+      pose.keypoints[0].position.y,
+      3,
+      3
+    );
+    //context.fillStyle = "white";
+    context.font = "20px Arial";
+    context.fillText("I got your nose!", 10, 30);
   });
