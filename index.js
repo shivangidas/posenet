56e26a5fa65948d9e4a9b7c57052848bfd423044
shivangi <shivangi.das7@gmail.com>
2018-08-11 00:38:39 +0530

    new poses

--- index.js
+++ index.js
@@ -10,15 +10,94 @@ canvas.width = imageData.width;
 posenet
   .load()
   .then(function(net) {
-    return net.estimateSinglePose(
+    return net.estimateMultiplePoses(
       imageData,
       imageScaleFactor,
       flipHorizontal,
       outputStride
     );
   })
-  .then(function(pose) {
-    console.log(pose);
+  .then(function(poseArray) {
+    console.log(poseArray);
+    context.strokeStyle = "red";
+    context.lineWidth = "5";
+    poseArray.map(pose => {
+      if (pose.score > 0.7) {
+        drawAline(
+          pose.keypoints[5].position.x,
+          pose.keypoints[5].position.y,
+          pose.keypoints[7].position.x,
+          pose.keypoints[7].position.y
+        );
+        drawAline(
+          pose.keypoints[5].position.x,
+          pose.keypoints[5].position.y,
+          pose.keypoints[6].position.x,
+          pose.keypoints[6].position.y
+        );
+        drawAline(
+          pose.keypoints[5].position.x,
+          pose.keypoints[5].position.y,
+          pose.keypoints[11].position.x,
+          pose.keypoints[11].position.y
+        );
+        drawAline(
+          pose.keypoints[12].position.x,
+          pose.keypoints[12].position.y,
+          pose.keypoints[6].position.x,
+          pose.keypoints[6].position.y
+        );
+        drawAline(
+          pose.keypoints[6].position.x,
+          pose.keypoints[6].position.y,
+          pose.keypoints[8].position.x,
+          pose.keypoints[8].position.y
+        );
+        drawAline(
+          pose.keypoints[7].position.x,
+          pose.keypoints[7].position.y,
+          pose.keypoints[9].position.x,
+          pose.keypoints[9].position.y
+        );
+        drawAline(
+          pose.keypoints[10].position.x,
+          pose.keypoints[10].position.y,
+          pose.keypoints[8].position.x,
+          pose.keypoints[8].position.y
+        );
+        drawAline(
+          pose.keypoints[11].position.x,
+          pose.keypoints[11].position.y,
+          pose.keypoints[12].position.x,
+          pose.keypoints[12].position.y
+        );
+
+        drawAline(
+          pose.keypoints[11].position.x,
+          pose.keypoints[11].position.y,
+          pose.keypoints[13].position.x,
+          pose.keypoints[13].position.y
+        );
+        drawAline(
+          pose.keypoints[14].position.x,
+          pose.keypoints[14].position.y,
+          pose.keypoints[12].position.x,
+          pose.keypoints[12].position.y
+        );
+        drawAline(
+          pose.keypoints[13].position.x,
+          pose.keypoints[13].position.y,
+          pose.keypoints[15].position.x,
+          pose.keypoints[15].position.y
+        );
+        drawAline(
+          pose.keypoints[14].position.x,
+          pose.keypoints[14].position.y,
+          pose.keypoints[16].position.x,
+          pose.keypoints[16].position.y
+        );
+      }
+    });
 
     /* context.fillRect(
       pose.keypoints[0].position.x,
@@ -26,7 +105,7 @@ posenet
       3,
       3
     ); */
-    context.beginPath();
+    /* context.beginPath();
     context.moveTo(
       pose.keypoints[0].position.x,
       pose.keypoints[0].position.y + 10
@@ -48,5 +127,11 @@ posenet
     );
     //context.fillStyle = "white";
     context.font = "20px Arial";
-    context.fillText("I got your nose!", 10, 30);
+    context.fillText("I got your nose!", 10, 30); */
   });
+function drawAline(x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+}
