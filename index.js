fd9048355190c1ddfee4903ef4a72a6cd9f43b32
shivangi <shivangi.das7@gmail.com>
2018-08-12 00:29:40 +0530

    added webcam

--- index.js
+++ index.js
@@ -2,110 +2,156 @@
 const imageScaleFactor = 0.5;
 const outputStride = 16;
 const flipHorizontal = false;
-let imageData = document.getElementById("image");
+//let imageData = document.getElementById("image");
+
 let canvas = document.getElementById("poseCanvas");
 let context = canvas.getContext("2d");
-canvas.height = imageData.height;
-canvas.width = imageData.width;
-posenet
-  .load()
-  .then(function(net) {
-    return net.estimateMultiplePoses(
-      imageData,
-      imageScaleFactor,
-      flipHorizontal,
-      outputStride
+let net, poseArray;
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+      "Browser API navigator.mediaDevices.getUserMedia not available"
     );
-  })
-  .then(function(poseArray) {
-    console.log(poseArray);
-    context.strokeStyle = "red";
-    context.lineWidth = "5";
-    poseArray.map(pose => {
-      if (pose.score > 0.7) {
-        drawAline(
-          pose.keypoints[5].position.x,
-          pose.keypoints[5].position.y,
-          pose.keypoints[7].position.x,
-          pose.keypoints[7].position.y
-        );
-        drawAline(
-          pose.keypoints[5].position.x,
-          pose.keypoints[5].position.y,
-          pose.keypoints[6].position.x,
-          pose.keypoints[6].position.y
-        );
-        drawAline(
-          pose.keypoints[5].position.x,
-          pose.keypoints[5].position.y,
-          pose.keypoints[11].position.x,
-          pose.keypoints[11].position.y
-        );
-        drawAline(
-          pose.keypoints[12].position.x,
-          pose.keypoints[12].position.y,
-          pose.keypoints[6].position.x,
-          pose.keypoints[6].position.y
-        );
-        drawAline(
-          pose.keypoints[6].position.x,
-          pose.keypoints[6].position.y,
-          pose.keypoints[8].position.x,
-          pose.keypoints[8].position.y
-        );
-        drawAline(
-          pose.keypoints[7].position.x,
-          pose.keypoints[7].position.y,
-          pose.keypoints[9].position.x,
-          pose.keypoints[9].position.y
-        );
-        drawAline(
-          pose.keypoints[10].position.x,
-          pose.keypoints[10].position.y,
-          pose.keypoints[8].position.x,
-          pose.keypoints[8].position.y
-        );
-        drawAline(
-          pose.keypoints[11].position.x,
-          pose.keypoints[11].position.y,
-          pose.keypoints[12].position.x,
-          pose.keypoints[12].position.y
-        );
+  }
 
-        drawAline(
-          pose.keypoints[11].position.x,
-          pose.keypoints[11].position.y,
-          pose.keypoints[13].position.x,
-          pose.keypoints[13].position.y
-        );
-        drawAline(
-          pose.keypoints[14].position.x,
-          pose.keypoints[14].position.y,
-          pose.keypoints[12].position.x,
-          pose.keypoints[12].position.y
-        );
-        drawAline(
-          pose.keypoints[13].position.x,
-          pose.keypoints[13].position.y,
-          pose.keypoints[15].position.x,
-          pose.keypoints[15].position.y
-        );
-        drawAline(
-          pose.keypoints[14].position.x,
-          pose.keypoints[14].position.y,
-          pose.keypoints[16].position.x,
-          pose.keypoints[16].position.y
-        );
-      }
-    });
+  let video = document.getElementById("video");
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "user",
+      width: canvas.width,
+      height: canvas.height
+    }
+  });
+  video.srcObject = stream;
 
-    /* context.fillRect(
+  return new Promise(resolve => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+async function loadVideo() {
+  const video = await setupCamera();
+  video.play();
+
+  return video;
+}
+function setupFPS() {
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom);
+}
+
+async function loadModel() {
+  net = await posenet.load();
+  console.log("Model loaded ...");
+}
+
+async function generatePose(video) {
+  poseArray = await net.estimateMultiplePoses(
+    video,
+    imageScaleFactor,
+    flipHorizontal,
+    outputStride
+  );
+  console.log(poseArray);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawSkeleton();
+}
+
+function drawSkeleton() {
+  context.strokeStyle = "red";
+  context.lineWidth = "5";
+  poseArray.map(pose => {
+    if (pose.score > 0.1) {
+      drawHead(
+        pose.keypoints[0].position.x,
+        pose.keypoints[0].position.y,
+        Math.abs(pose.keypoints[0].position.x - pose.keypoints[3].position.x)
+      );
+      drawAline(
+        pose.keypoints[5].position.x,
+        pose.keypoints[5].position.y,
+        pose.keypoints[7].position.x,
+        pose.keypoints[7].position.y
+      );
+      drawAline(
+        pose.keypoints[5].position.x,
+        pose.keypoints[5].position.y,
+        pose.keypoints[6].position.x,
+        pose.keypoints[6].position.y
+      );
+      drawAline(
+        pose.keypoints[5].position.x,
+        pose.keypoints[5].position.y,
+        pose.keypoints[11].position.x,
+        pose.keypoints[11].position.y
+      );
+      drawAline(
+        pose.keypoints[12].position.x,
+        pose.keypoints[12].position.y,
+        pose.keypoints[6].position.x,
+        pose.keypoints[6].position.y
+      );
+      drawAline(
+        pose.keypoints[6].position.x,
+        pose.keypoints[6].position.y,
+        pose.keypoints[8].position.x,
+        pose.keypoints[8].position.y
+      );
+      drawAline(
+        pose.keypoints[7].position.x,
+        pose.keypoints[7].position.y,
+        pose.keypoints[9].position.x,
+        pose.keypoints[9].position.y
+      );
+      drawAline(
+        pose.keypoints[10].position.x,
+        pose.keypoints[10].position.y,
+        pose.keypoints[8].position.x,
+        pose.keypoints[8].position.y
+      );
+      drawAline(
+        pose.keypoints[11].position.x,
+        pose.keypoints[11].position.y,
+        pose.keypoints[12].position.x,
+        pose.keypoints[12].position.y
+      );
+
+      drawAline(
+        pose.keypoints[11].position.x,
+        pose.keypoints[11].position.y,
+        pose.keypoints[13].position.x,
+        pose.keypoints[13].position.y
+      );
+      drawAline(
+        pose.keypoints[14].position.x,
+        pose.keypoints[14].position.y,
+        pose.keypoints[12].position.x,
+        pose.keypoints[12].position.y
+      );
+      drawAline(
+        pose.keypoints[13].position.x,
+        pose.keypoints[13].position.y,
+        pose.keypoints[15].position.x,
+        pose.keypoints[15].position.y
+      );
+      drawAline(
+        pose.keypoints[14].position.x,
+        pose.keypoints[14].position.y,
+        pose.keypoints[16].position.x,
+        pose.keypoints[16].position.y
+      );
+    }
+  });
+
+  /* context.fillRect(
       pose.keypoints[0].position.x,
       pose.keypoints[0].position.y,
       3,
       3
     ); */
-    /* context.beginPath();
+  /* context.beginPath();
     context.moveTo(
       pose.keypoints[0].position.x,
       pose.keypoints[0].position.y + 10
@@ -128,10 +174,26 @@ posenet
     //context.fillStyle = "white";
     context.font = "20px Arial";
     context.fillText("I got your nose!", 10, 30); */
-  });
+}
+
 function drawAline(x1, y1, x2, y2) {
   context.beginPath();
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
   context.stroke();
 }
+function drawHead(x, y, radius) {
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.stroke();
+}
+async function letTheGameBegin() {
+  loadModel();
+  let video = await loadVideo();
+  //setupFPS();
+  setInterval(function() {
+    generatePose(video);
+  }, 1000);
+}
+
+letTheGameBegin();
